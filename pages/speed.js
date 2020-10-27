1dd05a3c93602264ff9fe649a876d4a4fab72679
Ravikumar <ravikumar@Ravikumar-INKACTXM091.local>
2020-10-27 16:04:18 +0530

    Added lib icons

--- pages/speed.js
+++ pages/speed.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { GSTN_ICON } from "@shared/urls";
-import { Button, ZoidsRoot } from "@cleartax/zoids";
+import { ZoidsRoot, Message, Toast } from "@cleartax/zoids";
+// icons
+import { ArrowForward } from "@cleartax/zoids/build/lib/components/Icons/Icons";
 
 const Facebook = () => {
+  useEffect(() => {
+    const t = Toast.configure({});
+    console.log(t);
+  }, []);
   return (
     <ZoidsRoot>
       {GSTN_ICON}
-      <Button />
+      <ArrowForward />
+      <Message dismissText="close">Facebook</Message>
     </ZoidsRoot>
   );
 };
